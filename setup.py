b1eeccc75c2500e38709e0f957575fc3654ce308
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-08-22 16:14:03 +0200

    Add first work on the setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='fedmsg_pkgdb2branch',
+    version='0.1',
+    description='A fedmsg consumer that runs the pkgdb2branch based on fedmsg FAS messages',
+    license="LGPLv2+",
+    author='Janez Nemanič, Ralph Bean and Pierre-Yves Chibon',
+    author_email='admin@fedoraproject.org',
+    url='https://github.com/fedora-infra/fedmsg-pkgdb2branch',
+    install_requires=["fedmsg"],
+    packages=[],
+    py_modules=['fedmsg_pkgdb2branch'],
+    entry_points="""
+    [moksha.consumer]
+    fedmsg_pkgdb2branch = fedmsg_pkgdb2branch:FasClientConsumer
+    """,
+)
